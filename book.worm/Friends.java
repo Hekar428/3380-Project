05978410fb6cca8bda8de9179d02345407f45654
DementedArtist <blake.montalbano@gmail.com>
2014-04-30 21:42:04 -0500

    friends scanner updated

--- book.worm/Friends.java
+++ book.worm/Friends.java
@@ -2,24 +2,44 @@ package book.worm;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import java.util.Scanner;
 
 
 public class Friends extends JFrame implements ActionListener {
     
+    String SNumberOfFriends;
     JButton Main;
     JPanel pane = new JPanel();
-    int NEXTFRIEND;
+    File file = new File("LoggedInUser.txt");
+    int NumberOfFriends;
+    String NextFriend;
+    
     Friends(){
         super("Friends");
         Main = new JButton("Main Page");
         Main.addActionListener(this);
+        try{
+            Scanner keyb = new Scanner(file);
+            while (SNumberOfFriends!=";"){
+                SNumberOfFriends = keyb.next();
+            }
+            while (SNumberOfFriends!=";"){
+                SNumberOfFriends = keyb.next();
+            }
+            NumberOfFriends = Integer.parseInt(keyb.next());
 
-        JTextArea textArea = new JTextArea(User.NumberOfFriends, 16);
+        JTextArea textArea = new JTextArea(NumberOfFriends, 16);
         JScrollPane scrollPane = new JScrollPane(textArea); 
         textArea.setEditable(false);
-        for(int i=1; i<=User.NumberOfFriends; i++){
-            textArea.append(NEXTFRIEND + "//n");
+        for(int i=1; i<=NumberOfFriends; i++){
+            while(keyb.next()!= ","){
+                    NextFriend = NextFriend+keyb.next();
+                }
+        
+            textArea.append(NextFriend + "//n");
         }
+        } catch(FileNotFoundException s){}
         
         pane.add(Main);
         this.add(pane);
